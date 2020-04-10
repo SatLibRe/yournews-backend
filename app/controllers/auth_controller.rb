c01bdc87e2bc0ff3aad40b4cbf2a3ffcc0d82f3b
Miles Higbie <miles.higbie@gmail.com>
2020-04-10 12:38:58 -0400

    added login controller routes and method

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,11 @@
 class AuthController < ApplicationController
+
+    def login
+        user = User.find_by(name: params[:name])
+        if user && user.authenticate(params[:password]) 
+            render json: user
+        else 
+            render json: {errors: "Your Username or Password is inncorrect"}
+        end 
+    end 
 end
